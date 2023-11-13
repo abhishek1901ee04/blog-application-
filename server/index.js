f0a7be3096dec4703626b9a25bab65d6922cb379
abhishek1901ee04 <54908876+abhishek1901ee04@users.noreply.github.com>
2023-11-13 13:38:13 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', Router);
 
 
-const PORT = 8000;
+const PORT = process.env.PORT ||8000;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
